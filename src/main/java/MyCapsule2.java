7c590d77967906efbcbc9b33ae86806e4fde6d3f
Andrew Brown <a.c.brown@protonmail.com>
2020-01-14 18:47:54 -0600

    Fix jar name, remove commented code, add ignore file for Git

--- src/main/java/MyCapsule2.java
+++ src/main/java/MyCapsule2.java
@@ -1,13 +1,11 @@
-import java.nio.file.Path;
-
 /**
  * A custom capsule example
  */
 public class MyCapsule2 extends Capsule {
 
-	protected MyCapsule2(Capsule pred) {
-		super(pred);
-		System.out.println("My Custom Capsule2 class (pred)");
-	}
+    protected MyCapsule2(Capsule pred) {
+        super(pred);
+        System.out.println("My Custom Capsule2 class (pred)");
+    }
 
 }
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,9 @@
 package hello;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, World");
-		System.out.println("System-Properties:");
-		System.out.println("\t\\--propertyName1=" + System.getProperty("propertyName1"));
-		System.out.println("\t\\--propertyName2=" + System.getProperty("propertyName2"));
-//		final ImmutableCollection col = ImmutableList.of("hello","hello1"); // using guava lib
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 
 }
